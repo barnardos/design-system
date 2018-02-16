8da60d0d5c0c08d215400b5f4b7c32a0d4fcf18b
jeddy3 <jeddy3@users.noreply.github.com>
2018-02-16 11:50:38 +0000

    Add introduction
    Reworks the navigation to accommodate this.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Navigation from "../components/Navigation";
+import Paragraph from "../components/Paragraph";
 import Title from "../components/Title";
 
 import "./index.css";
@@ -7,6 +9,13 @@ import "./index.css";
 const Pages = () => (
   <div>
     <Title>{`Design system`}</Title>
+    <div className="Pages-paragraph">
+      <Paragraph
+      >{`Helping digital product teams create a consistent user experience and maximise efficiency.`}</Paragraph>
+    </div>
+    <div className="Pages-navigation">
+      <Navigation />
+    </div>
   </div>
 );
 
