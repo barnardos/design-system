4c7606e7c2105d8e48b45f2899a73c6bc55f48e0
jeddy3 <jeddy3@users.noreply.github.com>
2018-08-02 15:53:17 +0100

    Use shared stylelint config

--- stylelint/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  require("./stylelint-color-hex-whitelist"),
-  require("./stylelint-font-font-size-whitelist"),
-  require("./stylelint-function-arguments-whitelist"),
-  require("./stylelint-property-value-rem-multiples")
-];
