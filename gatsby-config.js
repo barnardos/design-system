a034da09f510802ba33344d02684239e9543563e
jeddy3 <jeddy3@users.noreply.github.com>
2018-08-07 16:21:10 +0100

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `${
+          process.env.CONTEXT !== "deploy-preview"
+            ? process.env.GOOGLE_ANALYTICS_ID
+            : "UA-000000-00"
+        }`,
+        head: false,
+        anonymize: true,
+        respectDNT: true
+      }
+    }
+  ]
 };
