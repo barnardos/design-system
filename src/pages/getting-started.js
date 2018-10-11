ab38521d294d583db7d5fa4c9f048ef493f0449b
jeddy3 <jeddy3@users.noreply.github.com>
2018-10-11 15:55:34 +0100

    Add contribution process

--- src/pages/getting-started.js
+++ src/pages/getting-started.js
@@ -3,10 +3,12 @@ import Helmet from "react-helmet";
 import React from "react";
 
 import Breadcrumbs from "../components/Breadcrumbs";
+import Heading from "../components/Heading";
 import Layout from "../components/Layout";
 import Link from "../components/Link";
 import ListItem from "../components/ListItem";
 import Paragraph from "../components/Paragraph";
+import TableOfContents from "../components/TableOfContents";
 import Title from "../components/Title";
 import UnorderedList from "../components/UnorderedList";
 
@@ -27,6 +29,15 @@ const Pages = () => (
       ]}
     />
     <Title>{`Getting started`}</Title>
+    <TableOfContents
+      items={[
+        {
+          level: 1,
+          text: "How to contribute",
+          to: "/getting-started/#how-to-contribute"
+        }
+      ]}
+    />
     <Paragraph>
       {`The Design System is for designers and developers working together.`}
     </Paragraph>
@@ -57,20 +68,18 @@ const Pages = () => (
     <Paragraph>
       {`If something isn't in the Design System, don't let it stop you. Design a solution, test it and use it in your product.`}
     </Paragraph>
+    <Heading>{`How to contribute`}</Heading>
     <Paragraph>
-      {`If you've created something you think solves a common problem and aligns with `}
-      <Link href="https://github.com/barnardos/design-system/blob/master/VISION.md">{`vision`}</Link>
-      {` on GitHub:`}
+      {`If you have a solution that could be included in the Design System:`}
     </Paragraph>
     <UnorderedList>
       <ListItem>
-        {`check for a `}
-        <Link href="https://github.com/barnardos/design-system/issues?q=is%3Aissue+is%3Aopen+label%3A%22type%3A+story%22">{`relevant issue`}</Link>
-        {` on GitHub, and give it a thumbs-up reaction`}
+        {`check whether a similar `}
+        <Link href="https://github.com/barnardos/design-system/issues?q=is%3Aissue+is%3Aopen+label%3A%22type%3A+story%22">{`solution is being discussed`}</Link>
       </ListItem>
       <ListItem>
-        <Link href="https://github.com/barnardos/design-system/issues/new?template=STORY.md">{`create an issue`}</Link>
-        {` if one doesn't exist`}
+        {`add a comment there or `}
+        <Link href="https://github.com/barnardos/design-system/issues/new?template=STORY.md">{`create a new issue`}</Link>
       </ListItem>
     </UnorderedList>
     <Paragraph>
