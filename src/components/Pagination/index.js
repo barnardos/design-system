62c4999e48c91392882be977255a8baac92a7c13
jeddy3 <jeddy3@users.noreply.github.com>
2019-02-11 12:21:56 +0000

    Refactor Pagination
    To remove code duplication.

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -4,40 +4,41 @@ import React from "react";
 
 import "./index.css";
 
+const PaginationItem = ({ direction, to, label }) => (
+  <li className="Pagination-item">
+    <GatsbyLink
+      className={`Pagination-link Pagination-link--${direction}`}
+      to={to}
+    >
+      <div className="Pagination-text">{`${direction[0].toUpperCase()}${direction.slice(
+        1
+      )}`}</div>
+      {label && <div className="Pagination-label">{label}</div>}
+    </GatsbyLink>
+  </li>
+);
+
 const Pagination = ({ previous, next }) => (
   <ul className="Pagination">
-    {previous && (
-      <li className="Pagination-item">
-        <GatsbyLink className="Pagination-link" to={previous.to}>
-          <div className="Pagination-text">{`Previous`}</div>
-          {previous.label && (
-            <div className="Pagination-label">{previous.label}</div>
-          )}
-        </GatsbyLink>
-      </li>
-    )}
-    {next && (
-      <li className="Pagination-item">
-        <GatsbyLink
-          className="Pagination-link Pagination-link--next"
-          to={next.to}
-        >
-          <div className="Pagination-text">{`Next`}</div>
-          {next.label && <div className="Pagination-label">{next.label}</div>}
-        </GatsbyLink>
-      </li>
-    )}
+    {previous && <PaginationItem direction="previous" {...previous} />}
+    {next && <PaginationItem direction="next" {...next} />}
   </ul>
 );
 
+PaginationItem.propTypes = {
+  direction: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  to: PropTypes.string.isRequired
+};
+
 Pagination.propTypes = {
   previous: PropTypes.shape({
-    to: PropTypes.string.isRequired,
-    label: PropTypes.string
+    label: PropTypes.string,
+    to: PropTypes.string.isRequired
   }),
   next: PropTypes.shape({
-    to: PropTypes.string.isRequired,
-    label: PropTypes.string
+    label: PropTypes.string,
+    to: PropTypes.string.isRequired
   })
 };
 
