b1fb3fcbfe84e1d8f2ea612d780b892d987852db
Joel Carr <git@joelcarr.co.uk>
2019-04-03 11:10:40 +0100

    Fix puppeteer default timeout

--- src/__tests__/a11y.test.js
+++ src/__tests__/a11y.test.js
@@ -5,12 +5,14 @@ const componentPages = ["content", "navigation", "forms", "structure"];
 describe("accessibility", () => {
   it("should have no violations for components", async () => {
     for (const href of componentPages) {
-      await page.goto(`http://localhost:8000/components/${href}`);
+      await page.goto(`http://localhost:8000/components/${href}`, {
+        timeout: 0
+      });
       const { violations } = await new AxePuppeteer(page)
         .disableRules("heading-order")
         .exclude(".algolia-autocomplete")
         .analyze();
       expect(violations).toHaveLength(0);
     }
-  }, 100000);
+  }, 1000000);
 });
